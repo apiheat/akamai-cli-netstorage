8d4ab99b903f9fd9050d3c31955260c3c65669c3
Petr Artamonov <partamonov@schubergphilis.com>
2018-02-21 14:37:31 +0100

    print out credentials source

--- config.go
+++ config.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/go-ini/ini"
+	"github.com/urfave/cli"
 )
 
 func config(configFile, configSection string) {
@@ -22,8 +23,15 @@ func config(configFile, configSection string) {
 			fmt.Printf("Section '%s' does not exist in %s. Please run '%s --section Your_Section_Name' ...\n", configSection, configFile, appName)
 			color.Unset()
 
+			cli.NewExitError(err.Error(), 1)
+
 			return
 		}
+
+		color.Set(color.FgBlue)
+		fmt.Printf("# Credentials used: [%s] from %s\n", configSection, configFile)
+		color.Unset()
+
 		nsHostname = section.Key("hostname").String()
 		nsKeyname = section.Key("keyname").String()
 		nsKey = section.Key("key").String()
